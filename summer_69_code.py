6b7c23327f48aa3de7cddaa00151312c6cf9cbdb
priya8920 <76489495+priya8920@users.noreply.github.com>
2021-01-15 12:57:03 +0530

    Update summer_69_code.py

--- summer_69_code.py
+++ summer_69_code.py
@@ -1,5 +1,5 @@
-#SUMMER OF '69: Return the sum of the numbers in the array,
-#except ignore sections of numbers starting with a 6 and extending to the next 9 (every 6 will be followed by at least one 9). Return 0 for no numbers
+#SUMMER OF '69: Return the sum of the numbers in the array,except ignore sections of numbers starting with a 6 and extending to the next 
+#                9 (every 6 will be followed by at least one 9). Return 0 for no numbers
 
 def summer_69(arr):
     sum=0
